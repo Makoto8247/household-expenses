a1a8ce9ff6238e5d4e43f651d007016c20f56031
Makoto8247 <48078578+Makoto8247@users.noreply.github.com>
2025-09-23 11:09:19 +0900

    Add Categories

--- app/db/db.js
+++ app/db/db.js
@@ -32,4 +32,45 @@ function getCategories() {
     return categoriesDB.all("SELECT name FROM categories");
 }
 
-export { createTables, getCategories };
+function addCategory(name, icon = null, color = "#65BBE9") {
+    try {
+        categoriesDB.execSQL(
+            "INSERT INTO categories (name, icon, color) VALUES (?, ?, ?)",
+            [name, icon, color]
+        );
+        return { success: true };
+    } catch (error) {
+        return { success: false, error: error.message };
+    }
+}
+
+function deleteCategory(id) {
+    try {
+        const count = categoriesDB.get(
+            "SELECT COUNT(*) FROM expenses WHERE category_id = ?",
+            [id]
+        );
+
+        if (count.count > 0) {
+            return { success: false, error: "Cannot delete category with associated expenses." };
+        }
+        categoriesDB.execSQL("DELETE FROM categories WHERE id = ?", [id]);
+        return { success: true };
+    } catch (error) {
+        return { success: false, error: error.message };
+    }
+}
+
+function updateCategory(id, name, icon = null, color = "#65BBE9") {
+    try {
+        categoriesDB.execSQL(
+            "UPDATE categories SET name = ?, icon = ?, color = ? WHERE id = ?",
+            [name, icon, color, id]
+        );
+        return { success: true };
+    } catch (error) {
+        return { success: false, error: error.message };
+    }
+}
+
+export { createTables, getCategories, addCategory, deleteCategory, updateCategory };
